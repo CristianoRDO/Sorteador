ee926aa91166ce624deba753fad2e5eb9da75ed9
CristianoRDO <crisrooliveira.2004@gmail.com>
2024-11-09 17:24:29 -0300

    Finalização do Projeto

--- app/src/main/java/br/edu/ifsp/dmo/sorteador/model/SorteioAdapter.kt
+++ app/src/main/java/br/edu/ifsp/dmo/sorteador/model/SorteioAdapter.kt
@@ -12,16 +12,18 @@ class SorteioAdapter(context: Context, datasource: List<Int>) :
     ArrayAdapter<Int>(context, R.layout.numero_sorteio, datasource) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var itemView = convertView
+        var itemView = convertView;
+
         if (itemView == null) {
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            itemView = inflater.inflate(R.layout.numero_sorteio, parent, false)
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater;
+            itemView = inflater.inflate(R.layout.numero_sorteio, parent, false);
         }
 
-        val number = getItem(position)
+        val number: Int? = getItem(position);
+
         if (itemView != null && number != null) {
-            itemView.findViewById<TextView>(R.id.posicao_numero).text = (position + 1).toString()
-            itemView.findViewById<TextView>(R.id.numero).text = number.toString()
+            itemView.findViewById<TextView>(R.id.posicao_numero).text = (position + 1).toString() + "° Sorteio: ";
+            itemView.findViewById<TextView>(R.id.numero).text = number.toString() + ".";
         }
 
         return itemView!!
